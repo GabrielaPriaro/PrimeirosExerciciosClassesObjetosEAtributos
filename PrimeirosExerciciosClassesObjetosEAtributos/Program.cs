d3529e7f41db62edafc9b4fc07cc99f2c163ab88
Gabriela <gabriela.priaro@gmail.com>
2024-11-25 17:47:00 +0000

    Add project files.

---
+++ PrimeirosExerciciosClassesObjetosEAtributos/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace PrimeirosExerciciosClassesObjetosEAtributos
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
